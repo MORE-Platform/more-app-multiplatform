d70fd9c1171d73d5191fa9f70010473447018147
Jan Cortiel <jan.cortiel@redlink.at>
2023-02-03 10:10:13 +0100

    MORE-572: Added iOS Login View Design

---
+++ iosApp/iosApp/Extensions/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  iosApp
+//
+//  Created by Jan Cortiel on 03.02.23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = Color.UI()
+    
+    struct UI {
+        let mainBackground = Color("MainBackground")
+        let mainTitle = Color("MainTitle")
+        let main = Color("Main")
+        let white = Color("White")
+    }
+}
