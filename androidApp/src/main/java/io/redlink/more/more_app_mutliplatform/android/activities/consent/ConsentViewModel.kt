171032a050cc9fa6bf09e1b01e1e3e00cc56890a
Daniil Barkov <daniil.barkov@redlink.at>
2023-03-01 13:39:56 +0100

    Added system permissions map, provided check within consent view

--- androidApp/src/main/java/io/redlink/more/more_app_mutliplatform/android/activities/consent/ConsentViewModel.kt
+++ androidApp/src/main/java/io/redlink/more/more_app_mutliplatform/android/activities/consent/ConsentViewModel.kt
@@ -1,13 +1,17 @@
 package io.redlink.more.more_app_mutliplatform.android.activities.consent
 
 import android.content.Context
+import android.os.Build
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.redlink.more.more_app_mutliplatform.android.extensions.getSecureID
+import io.redlink.more.more_app_mutliplatform.managers.ObservationsManager
 import io.redlink.more.more_app_mutliplatform.models.PermissionModel
+import io.redlink.more.more_app_mutliplatform.observations.ObservationType
 import io.redlink.more.more_app_mutliplatform.services.extensions.toMD5
 import io.redlink.more.more_app_mutliplatform.services.network.RegistrationService
+import io.redlink.more.more_app_mutliplatform.services.network.openapi.model.Study
 import io.redlink.more.more_app_mutliplatform.viewModels.permission.CorePermissionViewModel
 import kotlinx.coroutines.*
 
@@ -29,6 +33,9 @@ class ConsentViewModel(
         mutableStateOf(PermissionModel("Title", "Participation Info", emptyList()))
     val loading = mutableStateOf(false)
     val error = mutableStateOf<String?>(null)
+    val permissionsNotGranted = mutableStateOf(false)
+    private var study: Study? = null
+    val permissions = mutableSetOf<String>()
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
@@ -43,6 +50,26 @@ class ConsentViewModel(
             coreModel.loadingFlow.collect {
                 withContext(Dispatchers.Main) {
                     loading.value = it
+                }
+            }
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            study = registrationService.study
+
+            if (study != null) {
+                println("there is a study")
+            } else {
+                println("No study :(")
+            }
+            study?.observations?.let { observations ->
+                permissions += ObservationsManager
+                    .getAllPermissionsForFactories(observations.map { it.observationType })
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    ObservationType.NOTIFICATION.sensorPermission?.let { permissions.add(it) }
+                }
+                println("=-=-=-=-=-=-=-=-=")
+                permissions.forEach {
+                    println(it)
                 }
             }
         }
