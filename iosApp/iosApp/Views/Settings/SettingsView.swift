92e1ce5598cbfe418bcd26aeb69008d543eaa522
juliamayrhauser <julia.mayrhauser@redlink.at>
2023-03-08 16:08:39 +0100

    MORE-668 dummy implementation of settings view

---
+++ iosApp/iosApp/Views/Settings/SettingsView.swift
@@ -0,0 +1,60 @@
+//
+//  SettingsView.swift
+//  iosApp
+//
+//  Created by Julia Mayrhauser on 08.03.23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct SettingsView: View {
+    private let stringTable = "SettingsView"
+    var body: some View {
+        MoreMainBackgroundView {
+            VStack(alignment: .leading) {
+                Title(titleText: .constant(String.localizedString(forKey: "settings_title", inTable: stringTable, withComment: "Settings title")))
+                    .padding(.bottom)
+                Text(String.localizedString(forKey: "settings_text", inTable: stringTable, withComment: "information about accepted permissions"))
+                    .foregroundColor(.more.icons)
+                    .padding(.bottom)
+                Button {} label: {
+                    Text(String.localizedString(forKey: "refresh_study_config", inTable: stringTable, withComment: "button to refresh study configuration"))
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .foregroundColor(.more.white)
+                .background(Color.more.main)
+                .cornerRadius(.moreBorder.cornerRadius)
+                
+                ConsentList(permissionModel: .constant(PermissionModel(studyTitle: "Title", studyParticipantInfo: "Info", consentInfo: [
+                    PermissionConsentModel(title: "Study Consent", info: "Consent Info"),
+                    PermissionConsentModel(title: "Movement", info: "Accelerometer data"),
+                ])))
+                .padding(.top)
+                
+                Button {} label: {
+                    Text(String.localizedString(forKey: "leave_study", inTable: stringTable, withComment: "button to refresh study configuration"))
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .foregroundColor(.more.white)
+                .background(Color.more.important)
+                .cornerRadius(.moreBorder.cornerRadius)
+            }
+        } topBarContent: {
+            Button {
+                
+            } label: {
+                Image(systemName: "xmark")
+            }
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
---
+++ iosApp/iosApp/Views/Settings/SettingsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewModel.swift
+//  iosApp
+//
+//  Created by Julia Mayrhauser on 08.03.23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+
+class SettingsViewModel: ObservableObject {
+    private var coreSettingsViewModel: CoreSettingsViewModel? = nil
+    
+    func createCoreViewModel() {
+        
+    }
+}
