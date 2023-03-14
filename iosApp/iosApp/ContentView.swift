80a16ac04eb90f3c3b9a41fbd2edec860ee67a63
Jan Cortiel <jan.cortiel@redlink.at>
2023-03-14 20:06:58 +0100

    MORE-668: Added helper class for navigation view versions

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                         }
                     }
                 }
-                
+
             } topBarContent: {
                 HStack {
                     if viewModel.hasCredentials {
