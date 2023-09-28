47939f22472122d2b5c46e9b2243b2885f2feecb
Jan Cortiel <jan.cortiel@redlink.at>
2023-09-28 08:16:42 +0200

    Android Version Update: 2.3.9

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 2
-        versionName = "2.3.8"
+        versionName = "2.3.9"
     }
     buildFeatures {
         compose = true
