3bbd638a0b377fa2d1088cae39673b37aa3044db
Jan Cortiel <jan.cortiel@redlink.at>
2023-11-14 08:17:16 +0100

    Removal of keys and endpoints

--- shared/src/commonMain/kotlin/io/redlink/more/more_app_mutliplatform/services/store/EndpointRepository.kt
+++ shared/src/commonMain/kotlin/io/redlink/more/more_app_mutliplatform/services/store/EndpointRepository.kt
@@ -33,13 +33,9 @@ class EndpointRepository(private val sharedStorageRepository: SharedStorageRepos
 
     fun endpoint(): String = cache.ifEmpty { DATA_BASE_PATH_ENDPOINT }
 
-    fun loggingEndpoint(): String = LOGGING_ENDPOINT
-
     companion object {
         private const val ENDPOINT_KEY = "sharedStorageEndpointKey"
         private const val DATA_BASE_PATH_ENDPOINT: String =
             "https://data.more-health.at/api/v1"
-
-        private const val LOGGING_ENDPOINT: String = "https://1d2fb48a3e6e4da6aa8800f5b76711fd.us-central1.gcp.cloud.es.io:443/search-more-logs/_bulk"
     }
 }
