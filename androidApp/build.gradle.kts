f49327806ae9ad051896c8cb9083c5f05848153a
Alireza Fatehi <alireza.fatehi@dhp.lbg.ac.at>
2023-12-22 14:38:19 +0100

    new released version for relative study duration with data gateway on dev instance!

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 3
-        versionName = "3.0.0"
+        versionName = "3.0.3"
     }
     buildFeatures {
         compose = true
