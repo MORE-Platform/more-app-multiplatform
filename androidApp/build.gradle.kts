e6232b1878b00d3a6a29122087bf9453dba69794
Alireza Fatehi <107924035+alireza-dhp@users.noreply.github.com>
2024-02-15 16:33:47 +0100

    Update build.gradle.kts
    update verion "3.0.7"

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 3
-        versionName = "3.0.6"
+        versionName = "3.0.7"
     }
     buildFeatures {
         compose = true
