ef7b9ca8c7bceecafc1f00fb74e73e24d5d6a439
Jan Cortiel <jan.cortiel@redlink.at>
2024-04-11 14:28:59 +0200

    #208: Regex change and rewritten kotlin build gradle file to the newest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
     }
     repositories {
@@ -15,8 +14,8 @@ plugins {
     id("com.android.application").version("8.2.2").apply(false)
     id("com.android.library").version("8.2.2").apply(false)
     kotlin("android").version("1.9.22").apply(false)
-    kotlin("multiplatform").version("1.9.22").apply(false)
-    kotlin("plugin.serialization").version("1.9.22").apply(false)
+    kotlin("multiplatform").version("1.9.23").apply(false)
+    kotlin("plugin.serialization").version("1.9.23").apply(false)
 }
 
 
--- shared/src/commonMain/kotlin/io/redlink/more/more_app_mutliplatform/util/RegexData.kt
+++ shared/src/commonMain/kotlin/io/redlink/more/more_app_mutliplatform/util/RegexData.kt
@@ -3,6 +3,6 @@ package io.redlink.more.more_app_mutliplatform.util
 class RegexData {
     companion object {
         val url =
-            """http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"""
+            """https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"""
     }
 }
