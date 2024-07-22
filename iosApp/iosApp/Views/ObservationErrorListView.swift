6c1dca4c8e97fa57763b5700a94b953859353949
Jan Cortiel <jan.cortiel@redlink.at>
2024-07-22 12:08:48 +0200

    #263: Fixed an iOS TaskDetailsView Style

--- iosApp/iosApp/Views/ObservationErrorListView.swift
+++ iosApp/iosApp/Views/ObservationErrorListView.swift
@@ -10,7 +10,7 @@ import shared
 import SwiftUI
 
 struct ObservationErrorListView: View {
-     let taskObservationErrors: [String]
+    let taskObservationErrors: [String]
     let taskObservationErrorActions: [String]
 
     @State private var scrollViewContentSize: CGSize = .zero
@@ -18,47 +18,47 @@ struct ObservationErrorListView: View {
     private let navigationStrings = "Navigation"
 
     var body: some View {
-        if !taskObservationErrors.isEmpty {
-            ScrollView {
-                VStack {
-                    ForEach(taskObservationErrors, id: \.self) { error in
-                        HStack {
-                            Image(systemName: "exclamationmark.triangle")
-                                .font(.more.headline)
-                                .foregroundColor(.more.important)
-                                .padding(.trailing, 4)
-                            BasicText(text: "\(error.localize(withComment: "Error message", useTable: errorStrings))!")
+        if !taskObservationErrors.isEmpty || !taskObservationErrorActions.isEmpty {
+            VStack {
+                if !taskObservationErrors.isEmpty {
+                        ScrollView {
+                        VStack {
+                            ForEach(taskObservationErrors, id: \.self) { error in
+                                HStack {
+                                    Image(systemName: "exclamationmark.triangle")
+                                        .font(.more.headline)
+                                        .foregroundColor(.more.important)
+                                        .padding(.trailing, 4)
+                                    BasicText(text: "\(error.localize(withComment: "Error message", useTable: errorStrings))!")
+                                }
+                                .padding(.bottom)
+                            }
                         }
-                        .padding(.bottom)
                     }
+                    .frame(maxHeight: 100)
                 }
                 
-            }
-            if taskObservationErrorActions.isEmpty {
-                Divider()
-            }
-        }
-
-        if !taskObservationErrorActions.isEmpty {
-            if taskObservationErrorActions
-                .contains(Observation_.companion.ERROR_DEVICE_NOT_CONNECTED) {
-                MoreActionButton(disabled: .constant(false)) {
-                    ViewManager.shared.showBLEView(state: true)
-                } label: {
-                    HStack {
-                        Image(systemName: "applewatch")
-                            .foregroundColor(.more.white)
-                            .padding(.trailing, 4)
-                        Text(String.localize(forKey: "Devices", withComment: "Lists all connected or needed devices.", inTable: navigationStrings))
+                if !taskObservationErrorActions.isEmpty {
+                    if taskObservationErrorActions
+                        .contains(Observation_.companion.ERROR_DEVICE_NOT_CONNECTED) {
+                        MoreActionButton(disabled: .constant(false)) {
+                            ViewManager.shared.showBLEView(state: true)
+                        } label: {
+                            HStack {
+                                Image(systemName: "applewatch")
+                                    .foregroundColor(.more.white)
+                                    .padding(.trailing, 4)
+                                Text(String.localize(forKey: "Devices", withComment: "Lists all connected or needed devices.", inTable: navigationStrings))
+                            }
+                        }
                     }
                 }
             }
-
-            Divider()
         }
     }
 }
 
 #Preview {
-    ObservationErrorListView(taskObservationErrors: [], taskObservationErrorActions: [Observation_.companion.ERROR_DEVICE_NOT_CONNECTED])
+    ObservationErrorListView(taskObservationErrors: ["Error"], taskObservationErrorActions: [Observation_.companion.ERROR_DEVICE_NOT_CONNECTED])
+        
 }
