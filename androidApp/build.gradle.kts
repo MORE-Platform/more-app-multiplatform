5e6ee701e667f38aebe3e61fbcf8fddee94d5acc
Jan Cortiel <jan.cortiel@redlink.at>
2026-01-12 07:44:43 +0100

    Android Version Upgrade

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -49,8 +49,8 @@ android {
         applicationId = "io.redlink.umm.blendedcare"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.1.1"
+        versionCode = 3
+        versionName = "0.0.4"
     }
     buildFeatures {
         compose = true
