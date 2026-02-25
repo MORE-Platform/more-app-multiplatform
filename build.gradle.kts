0f3b1dce32c516c40e09d5443ac75250b89c383a
Jan Cortiel <jan.cortiel@redlink.at>
2026-02-25 11:04:33 +0100

    #217: Merge preparation

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,6 @@ buildscript {
     repositories {
         google()  // Google's Maven repository
         mavenCentral()  // Maven Central repository
-
-        maven {
-            url = uri("https://maven.pkg.github.com/redlink-gmbh/ignored")
-            credentials {
-                username = findProperty("io.redlink-gmbh.mvn.user") as String?
-                    ?: System.getenv("GITHUB_ACTOR")
-                password = findProperty("io.redlink-gmbh.mvn.key") as String?
-                    ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
     }
 }
 
