6bd4f8b068bc3bf10085270e77ef00a8309d824b
Jan Cortiel <jan.cortiel@redlink.at>
2026-03-09 15:13:03 +0100

    #291: Pipeline fix

--- setup_google_services.sh
+++ setup_google_services.sh
@@ -3,9 +3,9 @@
 # This script runs the fastlane setup_google_services lane for both iOS and Android.
 
 echo "Setting up Google Services for iOS..."
-(cd iosApp && fastlane setup_google_services)
+(cd iosApp/faslane && fastlane setup_google_services)
 
 echo "Setting up Google Services for Android..."
-(cd androidApp && fastlane setup_google_services)
+(cd androidApp/fastlane && fastlane setup_google_services)
 
 echo "Google Services setup completed."
